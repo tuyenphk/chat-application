4cf475af632fbff81e9fd6f03dca174158954149
Tuyen Pham <tuh39709@temple.edu>
2021-02-02 21:12:48 -0500

    update server side with users and router

--- server/server.js
+++ server/server.js
@@ -1,20 +1,27 @@
-let express = require('express')
-let http = require('http')
-let socket_IO = require('socket.io') 
+const express = require('express')
+const http = require('http')
+const socketIO = require('socket.io') 
+const cors = require('cors')
 
-const PORT = 8001 
-let app = express() 
-let server = http.Server(app) 
-let io = socketIO(server) 
+const {addUser, removeUser, getUser, getUsersInRoom} = require('./users')
+const router = require('router')
+
+const app = express() 
+const server = http.Server(app) 
+const io = socketIO(server)
+
+app.use(cors())
+app.use(router)
 
 io.on('connection', (socket) => {
-  console.log('User is connected sucessfully')
-  
-  socket.on('disconnect', () => {
-    console.log('Sorry! User is unfortunately disconnected')
-  })
+    
+    console.log('User is connected successfully')
+    
+    socket.on('disconnect', () => {
+        console.log('Sorry! User is unfortunately disconnected')
+    })
 })
 
-server.listen(PORT, () => {
-   console.log(`Running on port ${port}.`);
+server.listen(8001, () => {
+   console.log(`Running on port 8001`);
 });
