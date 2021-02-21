28da2cfc0d82c0a725fcdacdda60e7d15c0cf713
Tuyen Pham <tuh39709@temple.edu>
2021-02-21 15:28:32 -0500

    fix error on the front end

--- client/src/components/InfoBar/InfoBar.js
+++ client/src/components/InfoBar/InfoBar.js
@@ -1,18 +1,18 @@
-import React from 'react'
-import onlineIcon from '../../icons/onlineIcon.png'
-import closeIcon from '../../icons/closeIcon.png'
+import React from 'react';
 
-import './InfoBar.css'
-export const InfoBar = ({room}) => {
-    return(
-    <div className="infoBar">
-        <div className="leftInnerContainer">
-            <img className="onlineIcon" src={onlineIcon} alt="online icon" />
-            <h3>{room}</h3>
-        </div>
-        <div className="rightInnerContainer">
-            <a href='/'><img src={closeIcon} alt="closeIcon" /></a>
-        </div>
+import onlineIcon from '../../icons/onlineIcon.png';
+import closeIcon from '../../icons/closeIcon.png';
+
+import './InfoBar.css';
+
+export const InfoBar = ({ room }) => (
+  <div className="infoBar">
+    <div className="leftInnerContainer">
+      <img className="onlineIcon" src={onlineIcon} alt="online icon" />
+      <h3>{room}</h3>
     </div>
-    );
-}
+    <div className="rightInnerContainer">
+      <a href="/"><img src={closeIcon} alt="close icon" /></a>
+    </div>
+  </div>
+);
