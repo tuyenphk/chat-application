2edc801921044add4f224fa700d76e4a64040977
Tuyen Pham <tuh39709@temple.edu>
2021-02-21 15:48:16 -0500

    update style of chat box

--- client/src/components/TextContainer/TextContainer.js
+++ client/src/components/TextContainer/TextContainer.js
@@ -7,12 +7,12 @@ import './TextContainer.css';
 export const TextContainer = ({ users }) => (
   <div className='textContainer'>
     <div>
-      <h1>
+      <h1 style={{fontFamily: 'Source Code Pro', marginLeft: '-100px', marginTop: '100px'}}>
         Live Chat
         <br />
         Application
       </h1>
-      <h2>
+      <h2 style={{fontFamily: 'Arial', marginLeft: '-100px', fontSize: '25px'}}>
         This was built by
         <br />
         React, Express,
@@ -22,8 +22,8 @@ export const TextContainer = ({ users }) => (
     </div>
     {users ? (
       <div>
-        <h1>Users chatting</h1>
-        <div className='activeContainer'>
+        <h1 style={{fontFamily: 'Source Code Pro', marginLeft: '-100px', marginTop: '-300px', fontSize: '35px'}}>Users chatting</h1>
+        <div className='activeContainer' style={{fontFamily: 'Arial', marginLeft: '-100px', fontSize: '20px', marginTop: '-30px'}}>
           <h2>
             {users.map(({ name }) => (
               <div key={name} className='activeItem'>
